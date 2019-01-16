44a96d7f6b46136e7a1eceb5911d1e632f900cd1
jasperdezeeuw <jasper.dezeeuw@student.hu.nl>
2019-01-16 15:22:10 +0100

    Added documentation

--- src/EventManager.hpp
+++ src/EventManager.hpp
@@ -5,42 +5,54 @@
 #include <functional>
 #include <SFML/Graphics.hpp>
 
-
 class EventManager {
 private:
-	std::function< bool() > condition;
-	std::function< void() > work;
+	std::function< bool() > mCondition;
+	std::function< void() > mWork;
 public:
-	EventManager(
-		std::function< bool() > condition,
-		std::function< void() > work
-	) : condition(condition),
-		work(work)
+	/// \brief
+	/// Constructor for EventManager.
+	/// \param mCondition
+	/// This function is a condition to do the next function.
+	/// \param mWork
+	/// Needs a call for a void function.
+	EventManager(std::function< bool() > mCondition, std::function< void() > mWork) :
+		mCondition(mCondition),
+		mWork(mWork)
 	{}
 
-	EventManager(
-		sf::Keyboard::Key key,
-		std::function< void() > work
-	) :
-		condition(
-			[key]()->bool { return sf::Keyboard::isKeyPressed(key); }
-		),
-		work(work)
+	/// \brief
+	/// Extra constructor for keyboard key presses.
+	/// \details
+	/// This constructor returns what key is pressed on the keyboard.
+	/// \param mKey
+	/// Needs a key of the keyboard.
+	/// \param mWork
+	/// Needs a call for a void function.
+	EventManager(sf::Keyboard::Key mKey, std::function< void() > mWork) :
+		mCondition([mKey]()->bool { return sf::Keyboard::isKeyPressed(mKey); }),
+		mWork(mWork)
 	{}
 
-	EventManager(
-		sf::Mouse::Button button,
-		std::function< void() > work
-	) :
-		condition(
-			[button]()->bool { return sf::Mouse::isButtonPressed(button); }
-		),
-		work(work)
+
+	/// \brief
+	/// Extra constructor for mouse button presses.
+	/// \details
+	/// This constructor returns what button is pressed.
+	/// \param mButton
+	/// Needs a button of the mouse.
+	/// \param mWork
+	/// Needs a call for a void function.
+	EventManager(sf::Mouse::Button mButton, std::function< void() > mWork) :
+		mCondition([mButton]()->bool { return sf::Mouse::isButtonPressed(mButton); }),
+		mWork(mWork)
 	{}
 
+	/// \brief
+	/// Function for operator ().
 	void operator()() {
-		if (condition()) {
-			work();
+		if (mCondition()) {
+			mWork();
 		}
 	}
 };
