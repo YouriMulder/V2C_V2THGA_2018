52bd31d65061a0db159c7e83571ab44c0223c4cc
Deniz <deniz.lektemur@student.hu.nl>
2019-01-17 12:07:39 +0100

    Changed Platform to inherit from LevelObject

--- src/Entity/Platform.hpp
+++ src/Entity/Platform.hpp
@@ -2,18 +2,11 @@
 #define PLATFORM_HPP
 
 #include "SFML/Graphics.hpp"
-#include "EntityBase.hpp"
-#include "../ViewManager.hpp"
+#include "LevelObject.hpp"
+#include "ViewManager.hpp"
+#include <string>
 
-class Platform : public EntityBase {
-protected:
-	/// \brief
-	/// Image stored in a texture
-	sf::Texture mTexture;
-	
-	/// \brief
-	/// Sprite that contains a texture
-	sf::Sprite mSprite;
+class Platform : public LevelObject {
 public:
 	/// \brief
 	/// Constructor for Platform
@@ -27,7 +20,7 @@ public:
 	/// 
 	/// \param repeated
 	/// A boolean that decides if the picture is repeated default is false
-	Platform(const char * filename, const sf::Vector2f& position, const sf::Vector2f& size, const sf::IntRect& picturepart, int screenNumber, bool repeated = false);
+	Platform(const std::string& filename, const sf::Vector2f& position, const sf::Vector2f& size, const sf::IntRect& picturepart, int screenNumber, bool repeated = false);
 
 	/// \brief
 	/// Constructor for Platform
@@ -37,35 +30,7 @@ public:
 	/// Filename of the picture that is used as a texture for the platform
 	/// \param repeated
 	/// A boolean that decides if the picture is repeated default is false
-	Platform(const char * filename, const sf::Vector2f& position, const sf::Vector2f& size, int screenNumber, bool repeated = false);
-
-	/// \brief
-	/// Changes the size of the platform
-	void resize(float width, float height);
-
-	/// \brief
-	/// Changes the size of the platform
-	void resize(const sf::Vector2f& newSize);
-
-	/// \brief
-	/// Changes the width of the platform
-	void resizeWidth(float width);
-
-	/// \brief
-	/// Changes the height of the platform
-	void resizeHeight(float height);
-
-	/// \brief
-	/// Draws the platform in the given window
-	void draw(sf::RenderWindow& window) override;
-
-	/// \brief
-	/// Draws the platform in the given window
-	void draw(ViewManager& window) override;
-
-	/// \brief
-	/// Updates the position of the platform
-	void update() override;
+	Platform(const std::string& filename, const sf::Vector2f& position, const sf::Vector2f& size, int screenNumber, bool repeated = false);
 
 	/// \brief
 	/// Returns the outline of the platform
