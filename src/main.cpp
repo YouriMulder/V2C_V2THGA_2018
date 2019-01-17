16c4578b092b8ce0415fa362a9b91ce1d57a36c3
gerritvanos <31821855+gerritvanos@users.noreply.github.com>
2019-01-17 18:34:56 +0100

    cleaned up main and added txt/png files

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,3 @@
-#include <SFML/Audio.hpp>
-#include <SFML/Graphics.hpp>
-#include "Entity/Character.hpp"
-#include "Entity/EntityBase.hpp"
-#include <iostream>
-#include <memory>
-#include <vector>
-#include "Collision.hpp"
-#include "ViewManager.hpp"
-#include "action.hpp"
 #include "GameManager.hpp"
 int main() {
 	GameManager game = GameManager("../dependencies/levels/all.txt");
