3d17ae51397c176a0b5a8761e4491c0e2a10b000
gerritvanos <gerrit.vanos@student.hu.nl>
2019-01-22 13:10:39 +0100

    fixed character

--- src/Entity/Character.cpp
+++ src/Entity/Character.cpp
@@ -221,10 +221,6 @@ void Character::updateFacingDirection() {
 	}
 }
 
-bool Character::isFinished() {
-	return mIsFinished;
-}
-
 void Character::performAction(const Action& unperformedAction) {
 	for(auto& action : mActions) {
 		if(unperformedAction == action.first) {
