1fadfdae2169b01fa574360cdf23f0c2d7609858
Youri <Youri@demuldertjes.nl>
2019-01-23 17:39:34 +0100

    Windows sound support added in premake

--- premake5.lua
+++ premake5.lua
@@ -12,61 +12,61 @@ project "MindFuck"
 	filter { "system:windows" }
 		cppdialect "C++17"
 		systemversion "10.0.17763.0"
-		filter "configurations:*"
-			defines { "SFML_STATIC" }
-			filter "system:windows"
-				includedirs { "dependencies/SFML-2.5.1/include" }
-				libdirs { "dependencies/SFML-2.5.1/lib" }
+	filter "configurations:*"
+		defines { "SFML_STATIC" }
+	filter "system:windows"
+		includedirs { "dependencies/SFML-2.5.1/include" }
+		libdirs { "dependencies/SFML-2.5.1/lib" }
 
-			filter { "system:linux" }
-				links {
-					"sfml-graphics", 
-					"sfml-window", 
-					"sfml-system",
-					"sfml-audio"
-				}
+	filter { "system:linux" }
+		links {
+			"sfml-graphics", 
+			"sfml-window", 
+			"sfml-system",
+			"sfml-audio"
+		}
 
-		filter { "configurations:Debug" }
-			defines { "DEBUG" }
-			symbols "On"
-			filter "system:windows"
-				links {	
-					"sfml-graphics-s-d.lib",
-					"sfml-window-s-d.lib",
-					"sfml-system-s-d.lib",
-					"opengl32.lib",
-					"freetype.lib",
-					"winmm.lib",
-					"gdi32.lib",
-					"openal32.lib",
-					"flac.lib",
-					"vorbisenc.lib",
-					"vorbisfile.lib",
-					"vorbis.lib",
-					"ogg.lib",
-					"ws2_32.lib",
-					"sfml-audio-s-d.lib",
-				}
-		
-		filter { "configurations:Release" }
-			defines { "NDEBUG" }
-			optimize "On"
-			filter "system:windows"
-				links {	
-					"sfml-graphics-s.lib",
-					"sfml-window-s.lib",
-					"sfml-system-s.lib",
-					"opengl32.lib",
-					"freetype.lib",
-					"winmm.lib",
-					"gdi32.lib",
-					"openal32.lib",
-					"flac.lib",
-					"vorbisenc.lib",
-					"vorbisfile.lib",
-					"vorbis.lib",
-					"ogg.lib",
-					"ws2_32.lib",
-					"sfml-audio-s.lib",
-				}		
+	filter { "configurations:Debug" }
+		defines { "DEBUG" }
+		symbols "On"
+	filter { "configurations:Debug", "system:windows" }
+		links {	
+			"sfml-graphics-s-d.lib",
+			"sfml-window-s-d.lib",
+			"sfml-system-s-d.lib",
+			"opengl32.lib",
+			"freetype.lib",
+			"winmm.lib",
+			"gdi32.lib",
+			"openal32.lib",
+			"flac.lib",
+			"vorbisenc.lib",
+			"vorbisfile.lib",
+			"vorbis.lib",
+			"ogg.lib",
+			"ws2_32.lib",
+			"sfml-audio-s-d.lib",
+		}
+	
+	filter { "configurations:Release" }
+		defines { "NDEBUG" }
+		optimize "On"
+	filter { "system:windows", "system:windows" }
+		links {	
+			"sfml-graphics-s.lib",
+			"sfml-window-s.lib",
+			"sfml-system-s.lib",
+			"opengl32.lib",
+			"freetype.lib",
+			"winmm.lib",
+			"gdi32.lib",
+			"openal32.lib",
+			"flac.lib",
+			"vorbisenc.lib",
+			"vorbisfile.lib",
+			"vorbis.lib",
+			"ogg.lib",
+			"ws2_32.lib",
+			"sfml-audio-s.lib",
+		}		
 	
