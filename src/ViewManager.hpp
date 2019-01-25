4797693f077ac025865d9068406480c7827d598e
gerritvanos <31821855+gerritvanos@users.noreply.github.com>
2019-01-25 10:21:20 +0100

    added dots

--- src/ViewManager.hpp
+++ src/ViewManager.hpp
@@ -16,7 +16,7 @@ struct screen {
 class ViewManager{
 private:
 	/// \brief
-	/// a reference to the main window
+	/// a reference to the main window.
 	sf::RenderWindow & mMainWindow;
 
 	int mAmountOfScreens;
@@ -24,40 +24,40 @@ private:
 	sf::Vector2f mOffset = sf::Vector2f(20.f, 20.f);
 
 	/// \brief
-	/// A vector that contains all the separate screens
+	/// A vector that contains all the separate screens.
 	/// \details
 	/// This vector contains screens on the basis of the screen struct.
-	/// The most screens at this point in time is 4
+	/// The most screens at this point in time is 4.
 	std::vector<screen> mScreens;
 
 	std::array<sf::RectangleShape, 4> mBorders;
 	
 	/// \brief
-	/// Function that converts a given sf::Vector2u to a Vector2f
+	/// Function that converts a given sf::Vector2u to a Vector2f.
 	sf::Vector2f convertVector2u(const sf::Vector2u & input);
 	
 
 public:
 	/// \brief
-	/// Constructor for the ViewManager
+	/// Constructor for the ViewManager.
 	/// \details
-	/// contructs the ViewManager on basis of the parameters
+	/// contructs the ViewManager on basis of the parameters.
 	/// \param mainWindow
-	/// The window on wich de ViewManager is based
+	/// The window on wich de ViewManager is based.
 	/// \param noOffScreens
-	/// The number of screens that need to be made choices: 1,2,4
+	/// The number of screens that need to be made choices: 1,2,4.
 	ViewManager(sf::RenderWindow & mainWindow, const int & noOfScreens);
 
 	/// \brief
-	/// Function to select a certain screen for moving
+	/// Function to select a certain screen for moving.
 	/// \details
-	/// This function selects a scren note: multiple screens can be selected at once
+	/// This function selects a scren note: multiple screens can be selected at once.
 	/// \param screenNumber
 	/// The number of the screen you want to select.
 	void selectMoveScreen(const int & screenNumber);
 
 	/// \brief
-	/// Function to select a certain screen for drawing
+	/// Function to select a certain screen for drawing.
 	/// \details
 	/// Use this function before you draw to select where you want the object to be draw.\n
 	/// Giving 0 as a screen number selects the default view.
@@ -66,91 +66,91 @@ public:
 	void selectDrawingScreen(const int & screenNumber);
 
 	/// \brief
-	/// Fucntion to reset the view to the default view
+	/// Fucntion to reset the view to the default view.
 	void resetDrawingScreen();
 
 	/// \brief
-	/// Function to draw an sf::Drawable
+	/// Function to draw an sf::Drawable.
 	/// \details
 	/// \param drawable
-	/// The object that needs to de drawn
+	/// The object that needs to de drawn.
 	void draw(const sf::Drawable & drawable);
 
 	/// \brief
-	/// Function to draw vertices
+	/// Function to draw vertices.
 	/// \details
 	/// \param vertices
-	/// The vertices that needs to be draw
+	/// The vertices that needs to be draw.
 	/// \param vetexCount
-	/// The amount of vertices 
+	/// The amount of vertices .
 	/// \param type
-	/// The type of vertices for example sf::Lines
+	/// The type of vertices for example sf::Lines.
 	void draw(const sf::Vertex * vertices, std::size_t vertexCount, sf::PrimitiveType type);
 
 	/// \brief
-	/// Function to draw an sf::VertexBuffer
+	/// Function to draw an sf::VertexBuffer.
 	/// \details
 	/// \param vertexBuffer
-	/// The sf::VertexBuffer that needs to be drawn
+	/// The sf::VertexBuffer that needs to be drawn.
 	void draw(const sf::VertexBuffer & vertexBuffer);
 
 	/// \brief
-	/// Function to draw a part of an sf::VertexBuffer
+	/// Function to draw a part of an sf::VertexBuffer.
 	/// \details
 	/// \param vertexBuffer
-	/// The sf::VertexBuffer that needs to be drawn
+	/// The sf::VertexBuffer that needs to be drawn.
 	/// \param firstVertex
-	/// The vertex on wich the drawing should start
+	/// The vertex on wich the drawing should start.
 	/// \param vetexCount
-	/// The amount of vertices 
+	/// The amount of vertices.
 	void draw(const sf::VertexBuffer &vertexBuffer, std::size_t firstVertex, std::size_t vertexCount);
 
 	/// \brief
-	/// Function to move view(s)
+	/// Function to move view(s).
 	/// \details
-	/// This function moves all the selected views
+	/// This function moves all the selected views.
 	/// \param offset
-	/// The offset on basis of which the views need to be moved
+	/// The offset on basis of which the views need to be moved.
 	void move(const sf::Vector2f & offset);
 	
 	/// \brief
-	/// Adaptor function to display the window
+	/// Adaptor function to display the window.
 	void display();
 
 	/// \brief
-	/// Adaptor function to clear the window
+	/// Adaptor function to clear the window.
 	void clear();
 
 	/// \brief
-	/// Adaptor function to close the window
+	/// Adaptor function to close the window.
 	void close();
 
 	/// \brief
-	/// Adaptor function to check if the window isOpen
+	/// Adaptor function to check if the window isOpen.
 	/// \details
 	/// \return bool
-	/// returns true if window is open
+	/// returns true if window is open.
 	bool isOpen();
 
 	/// \brief
-	/// Adaptor function to poll events in the window
+	/// Adaptor function to poll events in the window.
 	bool pollEvent(sf::Event & e);
 
 
 	/// \brief
-	/// Function to get the current possition of the given view
+	/// Function to get the current possition of the given view.
 	/// \details
 	/// \param screenNumber
-	/// The number of the screen you want to get the position 
-	/// \return return the position of the view
+	/// The number of the screen you want to get the position .
+	/// \return return the position of the view.
 	sf::Vector2f getViewPosition(const int & screenNumber) const;
 
 	/// \brief
-	/// Function to get the current size of the given view
+	/// Function to get the current size of the given view.
 	/// \details
 	/// \param screenNumber
-	/// The number of the screen you want to get the size
-	/// \return return the size of the view
+	/// The number of the screen you want to get the size.
+	/// \return return the size of the view.
 	sf::Vector2f getViewSize(const int & screenNumber) const;
 
 	/// \brief
@@ -176,7 +176,7 @@ public:
 	/// \details
 	/// Bordor color is Red when selected.
 	/// \param screenNumber
-	/// The screen number of the screen the color needs to be set
+	/// The screen number of the screen the color needs to be set.
 	void setBordorColor(int screenNumber);
 
 	/// \brief
@@ -184,7 +184,7 @@ public:
 	/// \details
 	/// Bordor color is White when deselected.
 	/// \param screenNumber
-	/// The screen number of the screen the color needs to be set
+	/// The screen number of the screen the color needs to be set.
 	void resetBordorColor(int screenNumber);
 
 	virtual ~ViewManager();
