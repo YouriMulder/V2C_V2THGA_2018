718b9199ce5234e19a88a95651e438b8a048a824
Lars Versteeg <lars.versteeg1999@gmail.com>
2019-01-25 10:31:10 +0100

    Update ViewManager.hpp

--- src/ViewManager.hpp
+++ src/ViewManager.hpp
@@ -33,7 +33,7 @@ private:
 	std::array<sf::RectangleShape, 4> mBorders;
 	
 	/// \brief
-	/// Function that converts a given sf::Vector2u to a Vector2f.
+	/// Function that converts a given sf::Vector2u to a sf::Vector2f.
 	sf::Vector2f convertVector2u(const sf::Vector2u & input);
 	
 
@@ -41,17 +41,17 @@ public:
 	/// \brief
 	/// Constructor for the ViewManager.
 	/// \details
-	/// contructs the ViewManager on basis of the parameters.
+	/// Contructs the ViewManager on basis of the parameters.
 	/// \param mainWindow
-	/// The window on wich de ViewManager is based.
+	/// The window on which de ViewManager is based.
 	/// \param noOffScreens
-	/// The number of screens that need to be made choices: 1,2,4.
+	/// The number of screens that need to be made. Choices: 1,2,4.
 	ViewManager(sf::RenderWindow & mainWindow, const int & noOfScreens);
 
 	/// \brief
 	/// Function to select a certain screen for moving.
 	/// \details
-	/// This function selects a scren note: multiple screens can be selected at once.
+	/// This function selects a screen note: multiple screens can be selected at once.
 	/// \param screenNumber
 	/// The number of the screen you want to select.
 	void selectMoveScreen(const int & screenNumber);
@@ -59,14 +59,14 @@ public:
 	/// \brief
 	/// Function to select a certain screen for drawing.
 	/// \details
-	/// Use this function before you draw to select where you want the object to be draw.\n
+	/// Use this function before you draw to select where you want the object to be drawn.\n
 	/// Giving 0 as a screen number selects the default view.
 	/// \param screenNumber
 	/// The number of the screen you want to select.
 	void selectDrawingScreen(const int & screenNumber);
 
 	/// \brief
-	/// Fucntion to reset the view to the default view.
+	/// Function to reset the view to the default view.
 	void resetDrawingScreen();
 
 	/// \brief
@@ -80,9 +80,9 @@ public:
 	/// Function to draw vertices.
 	/// \details
 	/// \param vertices
-	/// The vertices that needs to be draw.
-	/// \param vetexCount
-	/// The amount of vertices .
+	/// The vertices that need to be draw.
+	/// \param vertexCount
+	/// The amount of vertices.
 	/// \param type
 	/// The type of vertices for example sf::Lines.
 	void draw(const sf::Vertex * vertices, std::size_t vertexCount, sf::PrimitiveType type);
@@ -100,7 +100,7 @@ public:
 	/// \param vertexBuffer
 	/// The sf::VertexBuffer that needs to be drawn.
 	/// \param firstVertex
-	/// The vertex on wich the drawing should start.
+	/// The vertex on which the drawing should start.
 	/// \param vetexCount
 	/// The amount of vertices.
 	void draw(const sf::VertexBuffer &vertexBuffer, std::size_t firstVertex, std::size_t vertexCount);
@@ -126,10 +126,10 @@ public:
 	void close();
 
 	/// \brief
-	/// Adaptor function to check if the window isOpen.
+	/// Adaptor function to check if the window is open.
 	/// \details
 	/// \return bool
-	/// returns true if window is open.
+	/// Returns true if window is open.
 	bool isOpen();
 
 	/// \brief
@@ -138,23 +138,23 @@ public:
 
 
 	/// \brief
-	/// Function to get the current possition of the given view.
+	/// Function to get the current position of the given view.
 	/// \details
 	/// \param screenNumber
-	/// The number of the screen you want to get the position .
-	/// \return return the position of the view.
+	/// The number of the screen you want to get the position from.
+	/// \return Return the position of the view.
 	sf::Vector2f getViewPosition(const int & screenNumber) const;
 
 	/// \brief
 	/// Function to get the current size of the given view.
 	/// \details
 	/// \param screenNumber
-	/// The number of the screen you want to get the size.
-	/// \return return the size of the view.
+	/// The number of the screen you want to get the size from.
+	/// \return Return the size of the view.
 	sf::Vector2f getViewSize(const int & screenNumber) const;
 
 	/// \brief
-	/// Function to get the current Amount of screens present in ViewMangager.
+	/// Function to get the current amount of screens present in ViewManager.
 	/// \details
 	/// \return Returns the current amount of screens.
 	int getAmountOfScreens();
@@ -162,7 +162,7 @@ public:
 	/// \brief
 	/// Function to change the amount of screens.
 	/// \details
-	/// This function changes the amount of screens dynamicly.
+	/// This function changes the amount of screens dynamically.
 	/// \param newAmount
 	/// The new amount of screens that need to be created.
 	void changeAmountOfScreens(int newAmount);
