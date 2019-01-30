8082076af7e607490b09ae2ef337474dde40d178
Lars <lars.versteeg1999@gmail.com>
2019-01-30 10:31:31 +0100

    Initial commit SoundPlayer.

---
+++ src/SoundPlayer.cpp
@@ -0,0 +1,17 @@
+#include "SoundPlayer.hpp"
+#include <iostream>
+#include <string>
+
+const std::string Sounds::hurt = "hurt.ogg";
+const std::string Sounds::death = "death.ogg";
+
+void SoundPlayer::play(const std::string &filename) {
+	mCurrentSound.stop();
+	makeBuffer(filename);
+	mCurrentSound.setBuffer(mBuffer);
+	mCurrentSound.play();
+}
+
+void SoundPlayer::makeBuffer(const std::string &filename) {
+	mBuffer.loadFromFile(mPathSound + filename);
+}
